43170749b708d419557b22e8cb85c00b52c5b9bf
bryancarias <bcarias84@gmail.com>
2026-02-10 22:06:22 -0600

    Add: Demo writing the application logic in the request handler

---
+++ GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace GloboTicket.TicketManagement.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LasModifiedDate { get; set; }
+    }
+}
